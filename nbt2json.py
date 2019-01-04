311f92eb21d849e59bc32445dff0036f52475689
Martmists <martmists@gmail.com>
2019-01-04 13:38:22 +0100

    Create nbt2json
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ nbt2json.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import json
+import sys
+from collections.abc import MutableMapping, Sequence
+from typing import Union, Any
+
+from argparse import ArgumentParser
+from nbt.nbt import NBTFile, _TAG_Numeric, TAG_String
+
+
+def _to_py(x: Any) -> Union[dict, str, list, int]:
+    if isinstance(x, (str, int)):
+        return x
+    if isinstance(x, (_TAG_Numeric, TAG_String)):
+        return x.value
+    if isinstance(x, (list, Sequence)):
+        return [_to_py(y) for y in x]
+    if isinstance(x, (dict, MutableMapping)):
+        return {_to_py(k): _to_py(v)
+                for k, v in x.items()}
+
+
+def nbt_to_json(filename: str, **dumps_kwargs: dict) -> str:
+    nbt = NBTFile(filename)
+    py = _to_py(nbt)
+    return json.dumps(py, **dumps_kwargs)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--pretty", help="Pretty-print output", action="store_true")
+    parser.add_argument("file", help="NBT file to jsonify")
+
+    args = parser.parse_args()
+
+    kwargs = {"indent": 4} if args.pretty else {}
+
+    print(nbt_to_json(args.file, **kwargs))
